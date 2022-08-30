b0111ce4830629a10023a80bbc4f157aee10a2c4
Ken Preiser <kenpreiser@gmail.com>
2022-08-29 21:54:42 -0700

    Update ITCashBlocker.sol

--- ITCashBlocker.sol
+++ ITCashBlocker.sol
@@ -2,6 +2,4 @@ pragma solidity 0.8.7;
 
 interface ITCashBlocker {
     function init(address _receiver, address factory, address[] calldata banlist) external;
-    function setReceiver(address _newReceiver) external;
-    function setFactory(address _newFactory) external;
 }
